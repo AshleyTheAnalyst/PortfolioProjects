793798487bd26980719bcf06ec8e2a104f60a9bb
AshleyTheAnalyst <136721456+AshleyTheAnalyst@users.noreply.github.com>
2023-06-27 16:15:14 +0800

    Add files via upload

--- COVID-19_Data_Exploration_script_SQL.sql
+++ COVID-19_Data_Exploration_script_SQL.sql
@@ -10,32 +10,51 @@ FROM Project_covid.dbo.[owid-covid-data_Vaccinations]
 WHERE continent IS NOT NULL
 ORDER BY 3,4
 
- -- Calculation of the death percentage in your own country
+-- Total death count across the continents up to the cut-off date
+SELECT continent,SUM(new_deaths) AS total_death_count
+FROM Project_covid..[owid-covid-data_Deaths]
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY total_death_count DESC
+
+ -- Death percentage in your own country
  -- Convert nvarchar to numeric data type for calculation
 
 SELECT Location, date, total_cases, total_deaths, CONVERT(float, total_deaths) / CONVERT(float, total_cases) * 100 AS Death_percentage
 FROM Project_covid.dbo.[owid-covid-data_Deaths]
-WHERE Location like '%China'AND continent IS NOT NULL
+WHERE Location like '%China'
 ORDER BY 1,2
 
--- Calculation of the percentage of population infected with covid-19
+-- Percentage of population infected with covid-19
 
-SELECT Location, date, population, total_cases, CONVERT(float, total_deaths) / CONVERT(float, total_cases) * 100 AS Death_percentage
+SELECT Location, date, population, total_cases, CONVERT(float, total_deaths) / CONVERT(float, total_cases) * 100 AS percent_population_infected
 FROM Project_covid.dbo.[owid-covid-data_Deaths]
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
--- Caluculation of the highest infection rate compared to population of countries
--- The proportion of the population that has been affected by covid-19 = total case / population
--- The highest proportion of the population that has been affected by covid-19 = max of (total case / population)
+-- Highest infection rate compared to population
+--- The proportion of the population that has been affected by covid-19 = total case / population
+--- The highest proportion of the population that has been affected by covid-19 = max of (total case / population)
+--- To convert null values to 0, use the ISNULL function.
 
-SELECT location,population, MAX(CONVERT(int,total_cases)) AS highest_infection, MAX(CONVERT(int, total_cases) / population) * 100 AS prop_population_infected
+SELECT location, population,
+MAX(CONVERT(int, ISNULL(total_cases, 0))) AS #_highest_infection,
+MAX(CONVERT(int, ISNULL(total_cases, 0)) / population) * 100 AS highest_infected_rate
 FROM Project_covid.dbo.[owid-covid-data_Deaths]
 WHERE continent IS NOT NULL
 GROUP BY location, population
-ORDER BY prop_population_infected DESC
+ORDER BY highest_infected_rate DESC
 
--- Caluculation of the highest death rate compared to population of countries
+--- Select the data only collected on the first day of the month
+SELECT location, population,date,
+MAX(CONVERT(int, ISNULL(total_cases, 0))) AS #_highest_infection,
+MAX(CONVERT(int, ISNULL(total_cases, 0)) / population) * 100 AS highest_infected_rate
+FROM Project_covid.dbo.[owid-covid-data_Deaths]
+WHERE continent IS NOT NULL AND DATEPART(day, date) = 1
+GROUP BY location, population,date
+ORDER BY highest_infected_rate DESC
+
+-- Countries with highest death count per population
 
 SELECT location,MAX(CONVERT(int,total_deaths)) AS highest_death, MAX(CONVERT(int, total_deaths) / population)* 100 AS total_death_rate
 FROM Project_covid.dbo.[owid-covid-data_Deaths]
@@ -51,15 +70,14 @@ WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY highest_death DESC
 
--- Total cases across the world
-SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases) * 100 AS deathpercentage
+-- Global numbers
+SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases) * 100 AS death_percentage
 FROM Project_covid.dbo.[owid-covid-data_Deaths]
 ORDER BY 1,2
 
--- Caluculation of the percentage of population that has recieved at least one covid vaccine
-
+-- Percentage of population that has recieved at least one covid vaccine
 --- Merge two files
----  Convert the data type to bigint since the value is too large for the data type (int) of the column
+--- Convert the data type to bigint since the value is too large for the data type (int) of the column
 --- Use CTE to solve the error that using the '#_people_vaccinated column' that just created for calculation
 WITH pop_vs_Vaccine (continent, location, population, #_people_vaccinated)
 AS
